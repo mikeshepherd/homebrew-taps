bcb4029e620960cc829667178762dcfc630baa43
Mark Wolfe <mark@wolfe.id.au>
2016-08-09 17:33:20 +1000

    Updated unicreds to 1.4.0

--- unicreds.rb
+++ unicreds.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Unicreds < Formula
   homepage 'https://github.com/versent/unicreds'
-  url 'https://github.com/Versent/unicreds/releases/download/v1.2.1/unicreds_1.2.1_darwin_x86_64.tgz'
-  version '1.2.1'
-  sha1 'e31dc23f26ca01cb5f7d7ac4d401daf69ac96fcd'
+  url 'https://github.com/Versent/unicreds/releases/download/v1.4.0/unicreds_1.4.0_darwin_x86_64.tgz'
+  version '1.4.0'
+  sha1 '70bfdfd9491cec74916d44d29398d0650292fd1e'
 
   depends_on :arch => :x86_64
 
