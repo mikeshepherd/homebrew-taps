377616eb922def132fc19d6b8f8cca9afb304f27
Mark Wolfe <mark@wolfe.id.au>
2016-08-23 10:35:39 +1000

    Updated to use sha256 to fix warnings
    Fixes https://github.com/Versent/unicreds/issues/43

--- unicreds.rb
+++ unicreds.rb
@@ -4,7 +4,7 @@ class Unicreds < Formula
   homepage 'https://github.com/versent/unicreds'
   url 'https://github.com/Versent/unicreds/releases/download/v1.4.0/unicreds_1.4.0_darwin_x86_64.tgz'
   version '1.4.0'
-  sha1 '70bfdfd9491cec74916d44d29398d0650292fd1e'
+  sha256 '34fe09adaed63b54dd6334447f0824a4d73957bcb473fd2c3b1b1eb015c2ebb0'
 
   depends_on :arch => :x86_64
 
