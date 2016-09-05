dc0951d7be4080fd5622ac829e2703374b695792
Mark Wolfe <mark@wolfe.id.au>
2016-09-06 01:23:05 +1000

    Bump version to the 1.5.0 release

--- unicreds.rb
+++ unicreds.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Unicreds < Formula
   homepage 'https://github.com/versent/unicreds'
-  url 'https://github.com/Versent/unicreds/releases/download/v1.4.0/unicreds_1.4.0_darwin_x86_64.tgz'
-  version '1.4.0'
-  sha256 '34fe09adaed63b54dd6334447f0824a4d73957bcb473fd2c3b1b1eb015c2ebb0'
+  url 'https://github.com/Versent/unicreds/releases/download/v1.5.0/unicreds_1.5.0_darwin_x86_64.tgz'
+  version '1.5.0'
+  sha256 '7d29df6dd88f4af1e4a08066e47514489cd9a6184445ed2990fcd7b1a18177ca'
 
   depends_on :arch => :x86_64
 
