a40f30fd99f9a5f3adc2f3a96ac75b96d530338e
Mark Wolfe <mark@wolfe.id.au>
2016-09-20 08:39:44 +1000

    Updated saml2aws to 1.2.0

--- saml2aws.rb
+++ saml2aws.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Saml2aws < Formula
   homepage 'https://github.com/versent/saml2aws'
-  url 'https://github.com/Versent/saml2aws/releases/download/v1.1.0/saml2aws_1.1.0_darwin_x86_64.tgz'
-  version '1.1.0'
-  sha256 '914a243a10ecf5a48699c50c2891a2194fedc445aab9be3513d1d97d9a1dbc6f'
+  url 'https://github.com/Versent/saml2aws/releases/download/v1.2.0/saml2aws_1.2.0_darwin_x86_64.tgz'
+  version '1.2.0'
+  sha256 '77217c98ad11ff3f720af313647e4437120a59c901cb633a256e3b53950dd203'
 
   depends_on :arch => :x86_64
 
