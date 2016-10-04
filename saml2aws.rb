204281f9bd74e3e9c58c6c3680c71456e0ceac2b
Mark Wolfe <mark@wolfe.id.au>
2016-10-04 16:16:48 +1100

    Update saml2aws.rb

--- saml2aws.rb
+++ saml2aws.rb
@@ -4,7 +4,7 @@ class Saml2aws < Formula
   homepage 'https://github.com/versent/saml2aws'
   url 'https://github.com/Versent/saml2aws/releases/download/v1.3.0/saml2aws_1.3.0_darwin_x86_64.tgz'
   version '1.3.0'
-  sha256 '5fd3d9167c03c15a91a1a61708edb10d152bd12d5d151549a8d125fb0091fce9'
+  sha256 'a29f2ef9deac3879b231d0346cde92b697b3e078eb35ef4ad35fef28dcd16512'
 
   depends_on :arch => :x86_64
 
