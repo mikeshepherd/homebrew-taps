d1e7381ac1ca5ab5c86dccb5ab8469c816b1dec0
Mark Wolfe <mark@wolfe.id.au>
2016-11-09 11:24:32 +1100

    Revert to using sha256 for saml2aws

--- saml2aws.rb
+++ saml2aws.rb
@@ -4,7 +4,7 @@ class Saml2aws < Formula
   homepage 'https://github.com/versent/saml2aws'
   url 'https://github.com/Versent/saml2aws/releases/download/v1.3.1/saml2aws_1.3.1_darwin_amd64.tar.gz'
   version '1.3.1'
-  sha512 'd80cc1fa8ea98742e6b22bb800db55a77cb4c296c3106404464e96d7bdf3f7b47bf980cfc1b816272b36ff4c1dc7de0604af31cbbd52c9e4beafe30c02ba1be4'
+  sha256 '534479bc0e01fc80c819d927737327a61461f4e0c9a03991783e3cd9dd6996cf'
 
   depends_on :arch => :x86_64
 
