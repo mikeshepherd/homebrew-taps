bd78391654533f47cf3e044495dc45c0e984dbd3
Gregory Bonk <32458123+TechnicalMercenary@users.noreply.github.com>
2019-01-08 10:23:34 -0500

    Update to 2.12

--- saml2aws.rb
+++ saml2aws.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Saml2aws < Formula
   homepage 'https://github.com/versent/saml2aws'
-  url 'https://github.com/Versent/saml2aws/releases/download/v2.10.0/saml2aws_2.10.0_darwin_amd64.tar.gz'
-  version '2.10.0'
-  sha256 '515f905ebc73b684e3e68b0a30089bf99a0b1ef792c3de24547179778b9b2792'
+  url 'https://github.com/Versent/saml2aws/releases/download/v2.12.0/saml2aws_2.12.0_darwin_amd64.tar.gz'
+  version '2.12.0'
+  sha256 '19061c92f1673586a20d192be2295ea5a78902ef18ded2feaa2ca1fd117e3664'
 
   depends_on :arch => :x86_64
 
