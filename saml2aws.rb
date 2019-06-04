70720678bd92a461bad1146efc21848164ee9a6e
Maxime Deravet <maximede@users.noreply.github.com>
2019-06-04 16:53:09 -0700

    Update Saml2aws to 2.15.0

--- saml2aws.rb
+++ saml2aws.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Saml2aws < Formula
   homepage 'https://github.com/versent/saml2aws'
-  version '2.14.0'
+  version '2.15.0'
   if OS.mac?
-    url 'https://github.com/Versent/saml2aws/releases/download/v2.14.0/saml2aws_2.14.0_darwin_amd64.tar.gz'
-    sha256 'ca5b903eb0786c73d8f28627687841cba70056a2b56556105c04b4405b06d288'
+    url 'https://github.com/Versent/saml2aws/releases/download/v2.15.0/saml2aws_2.15.0_darwin_amd64.tar.gz'
+    sha256 'fc6712c5d22e3aa074736621a58a6f0d115255692128870e91816019be39b19f'
   elsif OS.linux?
-    url 'https://github.com/Versent/saml2aws/releases/download/v2.14.0/saml2aws_2.14.0_linux_amd64.tar.gz'
-    sha256 'd96b420ae12b4f8d9506cb1cfb3c53ee55e8a3fea4d5eb65c89adac8a6710cf4'
+    url 'https://github.com/Versent/saml2aws/releases/download/v2.15.0/saml2aws_2.15.0_linux_amd64.tar.gz'
+    sha256 'd4c717b49556a67e22f5e9c332a79ec02af86b7ee2f0eb452712b176db3d3647'
   end
 
   depends_on :arch => :x86_64
