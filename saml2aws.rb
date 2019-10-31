b02684517a9c2224ac46d2f0afff64989a59c412
Sjoerd Dijkstra <sjoerd_d60@hotmail.com>
2019-10-31 15:25:58 +0100

    Update shasums

--- saml2aws.rb
+++ saml2aws.rb
@@ -5,10 +5,10 @@ class Saml2aws < Formula
   version '2.19.0'
   if OS.mac?
     url 'https://github.com/Versent/saml2aws/releases/download/v2.19.0/saml2aws_2.19.0_darwin_amd64.tar.gz'
-    sha256 'cb12832804f90d3fc08e7c51d0c8ed2228e44476321f4e79887a59521935ff1f'
+    sha256 '775bcf7583d2293736c7bdb69a74dc32c6e8a999547a0628efb9e459fa32a907'
   elsif OS.linux?
     url 'https://github.com/Versent/saml2aws/releases/download/v2.19.0/saml2aws_2.19.0_linux_amd64.tar.gz'
-    sha256 'a7728e26c6379a3d0406f28286bd145a341a26b3840bc18bd0ce7ccb19419970'
+    sha256 'b64e0be3ba9fc398572f37f926cd8ef346eff7572c909eb450f759f371416506'
   end
 
   depends_on :arch => :x86_64
