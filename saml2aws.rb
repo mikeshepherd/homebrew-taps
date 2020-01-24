8cd85d9f870c6605198195d1b9d672f950ebd16b
Mark Wolfe <mark.wolfe@versent.com.au>
2020-01-24 12:04:31 +1100

    Update saml2aws.rb

--- saml2aws.rb
+++ saml2aws.rb
@@ -4,10 +4,10 @@ class Saml2aws < Formula
   homepage 'https://github.com/versent/saml2aws'
   version '2.22.0'
   if OS.mac?
-    url 'https://github.com/Versent/saml2aws/releases/download/v2.21.0/saml2aws_2.22.0_darwin_amd64.tar.gz'
+    url 'https://github.com/Versent/saml2aws/releases/download/v2.22.0/saml2aws_2.22.0_darwin_amd64.tar.gz'
     sha256 '044e17eaef8683f3c96f626934556c07326da75cc502bfb461b668c2e21c76e9'
   elsif OS.linux?
-    url 'https://github.com/Versent/saml2aws/releases/download/v2.21.0/saml2aws_2.22.0_linux_amd64.tar.gz'
+    url 'https://github.com/Versent/saml2aws/releases/download/v2.22.0/saml2aws_2.22.0_linux_amd64.tar.gz'
     sha256 '3961ac43e742e2725522583cfdb6a3dc932e6a33fc3a6286556f6aa6125725a8'
   end
 
